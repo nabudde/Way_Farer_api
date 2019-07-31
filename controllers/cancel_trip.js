04b1fcf53b35c01a65a27515107bf23a18d02ee8
nabudde <kaseddeblairebelinda@gmail.com>
2019-07-31 14:30:16 +0300

    ft(cancel-trip):admin can cancel trip[Delivers #167308285]

---
+++ controllers/cancel_trip.js
@@ -0,0 +1,16 @@
+const { trips } = require('../models');
+
+
+exports.cancel_trip=(req,res)=>{
+    const cancel_trip = trips.find(t => t.trip_id == req.params.trip_id);
+    if(!cancel_trip){
+        return res.status(400).send("entered trip_id is not available");      
+        };
+    res.json({
+        status : "success",
+        data : {
+            message : "Trip cancelled successfully",
+        }
+    });
+    
+}
--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,15 @@ const signupController = require('../controllers/signup');
 const signinController=require('../controllers/signin');
 const create_tripController=require('../controllers/trips');
 const alltripController=require('../controllers/trips');
+const cancel_tripController=require('../controllers/cancel_trip');
 
 
 router.route('/auth/signup').post(signupController.signup);
 router.route('/auth/signin').post(signinController.signin);
 router.route('/trips').post(middleware.token_verification,create_tripController.trips);
 router.route('/trips').get(alltripController.alltrips);
+router.route('/trips/:trip_id/cancel').patch(middleware.token_verification,cancel_tripController.cancel_trip);
+
 
 
 module.exports = router;
