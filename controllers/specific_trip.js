f05810d24599537b5a122c525074542b247e4a7e
nabudde <kaseddeblairebelinda@gmail.com>
2019-08-01 16:39:28 +0300

    ft(specific_trip):add a specific trip[Delivers #167308308]

---
+++ controllers/specific_trip.js
@@ -0,0 +1,15 @@
+const Joi =require ("joi");
+const { trips } = require('../models');
+exports.specific_trip = (req, res) => {
+    const specific_trip = trips.find(t => t.trip_id == req.params.trip_id);
+    if(!specific_trip){
+        return res.status(400).send("entered trip_id is not available");      
+        };
+    res.json({
+        status : "success",
+        data : {
+            specific_trip
+        }
+    });
+    
+}
