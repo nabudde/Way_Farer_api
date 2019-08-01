4c12bcc4c4e9b579867ee596050bcf40dee0bc63
nabudde <kaseddeblairebelinda@gmail.com>
2019-08-01 17:45:01 +0300

    ft(booking):book a seat on a trip[Delivers #167308310]

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,9 @@ const signinController=require('../controllers/signin');
 const create_tripController=require('../controllers/trips');
 const alltripController=require('../controllers/trips');
 const cancel_tripController=require('../controllers/cancel_trip');
-// const specific_tripController=require('../controllers/specific_trip')
-;
-const specific_tripController=require('../controllers/specific_trip')
+const specific_tripController=require('../controllers/specific_trip');
+const book_a_seatController=require('../controllers/book_a_seat');
+
 
 
 
@@ -18,6 +18,8 @@ router.route('/trips').post(middleware.token_verification,create_tripController.
 router.route('/trips').get(middleware.token_verification,alltripController.alltrips);
 router.route('/trips/:trip_id/cancel').patch(middleware.token_verification,cancel_tripController.cancel_trip);
 router.route('/trips/:trip_id').get(middleware.token_verification,specific_tripController.specific_trip);
+router.route('/bookings').post(middleware.token_verification,book_a_seatController.book_a_seat);
+
 
 
 
